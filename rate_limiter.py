2271eb5ea642cceefcad39640422508d912b025c
AbyssNode <AbyssNode123@gmail.com>
2025-05-29 15:01:10 +1000

    rate_limiter.py

---
+++ rate_limiter.py
@@ -0,0 +1,14 @@
+# rate_limiter.py
+import time
+
+class RateLimiter:
+    def __init__(self, delay=1.0):
+        self.delay = delay
+        self.last_call = 0
+
+    def wait(self):
+        now = time.time()
+        elapsed = now - self.last_call
+        if elapsed < self.delay:
+            time.sleep(self.delay - elapsed)
+        self.last_call = time.time()
